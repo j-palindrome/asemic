7a7532e65d76ba364d8401afc918a01a985009aa
Joshua Tazman Reinier <jreinier@oberlin.edu>
2024-12-25 02:56:11 -0800

    Revert "not working either"
    This reverts commit 4a41689019fb95ce7f8921251218cdc6e0d06f42.

--- examples/src/SimpleTest.tsx
+++ examples/src/SimpleTest.tsx
@@ -5,7 +5,7 @@ import Brush from '../../src/Brush'
 export default function SimpleTest() {
   return (
     <Asemic>
-      <Brush key={now()} render={b => b.newGroup().newCurve([0, 0], [0, 1])} />
+      <Brush key={now()} render={b => b.text('trying')} />
     </Asemic>
   )
 }

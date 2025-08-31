c8a63b8c6c8e03b18b856e384080347896d9411a
Jay Reinier <jtreinier@gmail.com>
2025-08-31 12:31:58 -0400

    expression parsing

--- src/parser/methods/Expressions.ts
+++ src/parser/methods/Expressions.ts
@@ -20,7 +20,10 @@ export class ExpressionMethods {
     '-',
     '*',
     '/',
-    '%'
+    '%',
+    '(',
+    ')',
+    ' '
   ]
   private static readonly OPERATOR_REGEX = new RegExp(
     `[${this.OPERATORS.map(x => `\\${x}`).join('')}]`,
@@ -52,6 +55,10 @@ export class ExpressionMethods {
     if (/[^\-\d\.]/.test(stringExpr)) {
       const foundKey = this.sortedKeys.find(x => stringExpr.startsWith(x))
       if (foundKey) {
+        if (stringExpr.includes(' ')) {
+          const [foundKey, ...args] = this.parser.tokenize(stringExpr)
+          return this.parser.constants[foundKey](...args)
+        }
         const arg1 = stringExpr.slice(foundKey.length)
         return this.parser.constants[foundKey](arg1)
       } else {
@@ -97,105 +104,112 @@ export class ExpressionMethods {
     let splitResult: { string: string; operatorType: string }[] =
       this.operatorSplitCache[expr]
     // Optimized parentheses handling
-    let parenIndex = expr.lastIndexOf('(')
-    while (parenIndex !== -1) {
-      let closeParen = expr.indexOf(')', parenIndex + 1)
-      const solvedExpr = expr.substring(parenIndex + 1, closeParen)
-      expr =
-        expr.substring(0, parenIndex) +
-        this.expr(solvedExpr).toFixed(4) +
-        expr.substring(closeParen + 1)
+    // make () operators, have the string split
+    // during evaluation check for () and operate accordingly!
+    // let parenIndex = expr.lastIndexOf('(')
+    // while (parenIndex !== -1) {
+    //   let closeParen = expr.indexOf(')', parenIndex + 1)
+    //   const solvedExpr = expr.substring(parenIndex + 1, closeParen)
+    //   expr =
+    //     expr.substring(0, parenIndex) +
+    //     this.expr(solvedExpr).toFixed(4) +
+    //     expr.substring(closeParen + 1)
 
-      parenIndex = expr.lastIndexOf('(')
-    }
+    //   parenIndex = expr.lastIndexOf('(')
+    // }
     let stringExpr = expr as string
 
     // NO PARENTHESES NOW
 
     // Cache space check
-    if (stringExpr.includes(' ')) {
-      const [funcName, ...args] = stringExpr.split(' ')
-      if (this.parser.constants[funcName]) {
-        return this.parser.constants[funcName](...args)
-      } else {
-        throw new Error(`Unknown function ${stringExpr}`)
-      }
-    } else if (stringExpr.includes('_')) {
-      const [funcName, ...args] = stringExpr.split('_')
+    // if (stringExpr.includes(' ')) {
+    //   const [funcName, ...args] = stringExpr.split(' ')
+    //   if (this.parser.constants[funcName]) {
+    //     return this.parser.constants[funcName](...args)
+    //   } else {
+    //     throw new Error(`Unknown function ${stringExpr}`)
+    //   }
+    // } else if (stringExpr.includes('_')) {
+    //   const [funcName, ...args] = stringExpr.split('_')
 
-      const foundKey = this.sortedKeys.find(x => funcName.startsWith(x))
-      if (foundKey) {
-        const arg1 = funcName.slice(foundKey.length).trim()
-        return this.parser.constants[foundKey](arg1, ...args)
-      } else {
-        throw new Error(`Unknown function ${stringExpr}`)
-      }
-    } else {
-      if (splitResult === undefined) {
-        // Find operator splits in the string
-        splitResult = []
-        ExpressionMethods.OPERATOR_REGEX.lastIndex = 0
-        let operators = stringExpr.matchAll(ExpressionMethods.OPERATOR_REGEX)
+    //   const foundKey = this.sortedKeys.find(x => funcName.startsWith(x))
+    //   if (foundKey) {
+    //     const arg1 = funcName.slice(foundKey.length).trim()
+    //     return this.parser.constants[foundKey](arg1, ...args)
+    //   } else {
+    //     throw new Error(`Unknown function ${stringExpr}`)
+    //   }
+    // } else {
+    if (splitResult === undefined) {
+      // Find operator splits in the string
+      splitResult = []
+      ExpressionMethods.OPERATOR_REGEX.lastIndex = 0
+      let operators = stringExpr.matchAll(ExpressionMethods.OPERATOR_REGEX)
 
-        let operatorArray = Array.from(operators)
+      let operatorArray = Array.from(operators)
 
-        for (let i = 0; i < operatorArray.length; i++) {
-          const match = operatorArray[i]
-          const operatorIndex = match.index!
+      for (let i = 0; i < operatorArray.length; i++) {
+        const match = operatorArray[i]
+        const operatorIndex = match.index!
 
-          // Add the text before this operator
-          if (i === 0) {
-            splitResult.push({
-              string: stringExpr.substring(0, operatorIndex),
-              operatorType: ''
-            })
-          }
-          // if the previous operator was immediately before this one, and this is a negative sign, we need to merge it with the previous operator
-          if (
-            match[0] === '-' &&
-            (operatorIndex === 0 ||
-              (i > 0 && operatorIndex === operatorArray[i - 1].index + 1))
-          ) {
-            last(splitResult)!.string += stringExpr.substring(
-              operatorIndex,
-              i < operatorArray.length - 1
-                ? operatorArray[i + 1].index
-                : stringExpr.length
-            )
-            continue
-          }
-
-          // Add the text after this operator with the operator type
+        // Add the text before this operator
+        if (i === 0) {
           splitResult.push({
-            string: stringExpr.substring(
-              operatorIndex + 1,
-              i < operatorArray.length - 1
-                ? operatorArray[i + 1].index
-                : stringExpr.length
-            ),
-            operatorType: match[0]
-          })
-        }
-
-        // If no operators found, add the entire string
-        if (operatorArray.length === 0) {
-          splitResult.push({
-            string: stringExpr,
+            string: stringExpr.substring(0, operatorIndex),
             operatorType: ''
           })
         }
-        // if (splitResult.length > 2) debugger
+        // if the previous operator was immediately before this one, and this is a negative sign, we need to merge it with the previous operator
+        if (
+          match[0] === '-' &&
+          (operatorIndex === 0 ||
+            (i > 0 && operatorIndex === operatorArray[i - 1].index + 1))
+        ) {
+          last(splitResult)!.string += stringExpr.substring(
+            operatorIndex,
+            i < operatorArray.length - 1
+              ? operatorArray[i + 1].index
+              : stringExpr.length
+          )
+          continue
+        }
 
-        this.operatorSplitCache[expr] = splitResult
+        // Add the text after this operator with the operator type
+        splitResult.push({
+          string: stringExpr.substring(
+            operatorIndex + 1,
+            i < operatorArray.length - 1
+              ? operatorArray[i + 1].index
+              : stringExpr.length
+          ),
+          operatorType: match[0]
+        })
       }
-      invariant(splitResult)
-      if (splitResult.length === 1) {
-        if (splitResult[0].string.length === 0)
-          throw new Error(`Empty expression beginning ${stringExpr}`)
-        return this.fastExpr(splitResult[0].string)
-      } else {
-        let leftVal: number = this.fastExpr(splitResult[0].string)
 
+      // If no operators found, add the entire string
+      if (operatorArray.length === 0) {
+        splitResult.push({
+          string: stringExpr,
+          operatorType: ''
+        })
+      }
+
+      this.operatorSplitCache[expr] = splitResult
+    }
+    invariant(splitResult)
+    if (splitResult.length === 1) {
+      if (splitResult[0].string.length === 0)
+        throw new Error(`Empty expression beginning ${stringExpr}`)
+      return this.fastExpr(splitResult[0].string)
+    } else {
+      const solveSplitResult = (splitResult: typeof splitResult2) => {
+        // if (splitResult.find(x => x.string.includes('hash'))) debugger
+        let leftVal: number = this.fastExpr(splitResult[0].string)
+        if (splitResult.find(x => x.operatorType === ' ')) {
+          return this.fastExpr(
+            splitResult.map(x => x.operatorType + x.string).join('')
+          )
+        }
         // Find the split index for the operator
         for (let i = 1; i < splitResult.length; i++) {
           const result = splitResult[i]
@@ -240,7 +254,28 @@ export class ExpressionMethods {
 
         return leftVal
       }
+      let splitResult2 = [...splitResult]
+      let index = splitResult2.findLastIndex(x => x.operatorType === '(')
+      while (index !== -1) {
+        const closingIndex = splitResult2.findIndex(
+          (x, i) => x.operatorType === ')' && i > index
+        )
+        if (closingIndex === -1)
+          throw new Error(`Mismatched parentheses in ${stringExpr}`)
+        splitResult2[index] = { ...splitResult2[index], operatorType: '' }
+        const innerResult = solveSplitResult(
+          splitResult2.slice(index, closingIndex)
+        )
+        splitResult2 = [
+          ...splitResult2.slice(0, index - 1),
+          { ...splitResult2[index - 1], string: innerResult.toString() },
+          ...splitResult2.slice(closingIndex + 1)
+        ]
+        index = splitResult2.findLastIndex(x => x.operatorType === '(')
+      }
+      return solveSplitResult(splitResult2)
     }
+    // }
   }
 
   choose(value0To1: string | number, ...callbacks: (() => void)[]) {

95534cf11fa2aece85f0fa23087fc2ba7e43a5fc
Jay Reinier <jtreinier@gmail.com>
2025-09-09 01:14:34 -0400

    finish ssl

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron/simple'
 import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), basicSsl()],
   build: {
     sourcemap: true,
     target: 'es2020'
@@ -15,5 +16,8 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, './src')
     }
+  },
+  server: {
+    host: true
   }
 })
